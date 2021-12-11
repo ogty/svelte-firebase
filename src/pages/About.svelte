d59b11087765d49674600252c42bf24b3ac1aba4
ogty <mofuiti1803@gmail.com>
2021-12-11 16:17:03 +0900

    add: design

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,5 +1,8 @@
-<p>
-    asdfsadfasdf<br>
-    asdfasdfasdf<br>
-    asfdasdfsafasdf<br>
-</p>
+
+<div style="height: 300px;">
+</div>
+<section class="text-center px-8">
+    <h2 class="text-gray-900 text-4xl tracking-tight font-extrabold sm:text-5xl">
+        “Best practices” don’t actually work.
+    </h2>
+</section>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
     import * as firebaseAuth from 'firebase/auth'
-    import UserPage from './UserPage.svelte';
-    import About from './About.svelte';
+    import UserPage from './UserPage.svelte'
+    import About from './About.svelte'
 
-    const auth = firebaseAuth.getAuth();
+    const auth = firebaseAuth.getAuth()
     let isUser = false
     let userIconURL = ""
 
     firebaseAuth.onAuthStateChanged(auth, (user) => {
         if (user) {
-            const displayName = user.displayName;
-            const photoURL = user.photoURL;
+            const displayName = user.displayName
+            const photoURL = user.photoURL
 
             console.log(displayName)
             console.log(photoURL)
@@ -23,12 +23,12 @@
 
     async function loginWithGoogle() {
         try {
-            const provider = new firebaseAuth.GoogleAuthProvider();
-            await firebaseAuth.signInWithPopup(auth, provider);
-            // await signInWithRedirect(auth, provider);
+            const provider = new firebaseAuth.GoogleAuthProvider()
+            // await firebaseAuth.signInWithPopup(auth, provider);
+            await firebaseAuth.signInWithRedirect(auth, provider)
             console.log("Success!!")
         } catch (e) {
-            console.log(e);
+            console.log(e)
         }
     }
 
@@ -37,14 +37,35 @@
             await firebaseAuth.signOut(auth)
             location.reload()
         } catch (e) {
-            console.log(e);
+            console.log(e)
         }
     }
 </script>
 
+    
 {#if isUser}
     <UserPage {logOut} {userIconURL} />
 {:else}
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={loginWithGoogle}>Log in</button>
+    <header class="text-gray-100body-font w-full h-100">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <a href="/" class="text-2xl">Logo Space</a>
+            <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+            </nav>
+            <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+                <a href="/" class="ml-4 py-2 px-3 font-bold">
+                    <span class="text-slate-900">Company</span>
+                </a>
+                <a href="/" class="ml-4 py-2 px-3 font-bold">
+                    <span class="text-slate-900">Pricing</span>
+                </a>
+                <a href="#" class="ml-4 py-2 px-3 font-bold" on:click={loginWithGoogle}>
+                    <span class="text-slate-900">Log in</span>
+                </a>
+                <a href="/" class="no-underline transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300 bg-zinc-900 font-bold ml-4 py-2 px-3 rounded-lg">
+                    <span class="text-white">Sign up</span>
+                </a>
+            </div>
+        </div>
+    </header>
     <About />
 {/if}
