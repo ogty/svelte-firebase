1d282c6b9f1cf06df43fe2fa2c26f6a4022e220c
ogty <mofuiti1803@gmail.com>
2021-12-11 19:08:28 +0900

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -4,14 +4,13 @@
     import { initializeApp }  from 'firebase/app';
 
 	const firebaseConfig = {
-        apiKey: "AIzaSyApoCd_j5OKnskS1CFSidlmeCEBCpB2oKM",
-        authDomain: "test-2b296.firebaseapp.com",
-        projectId: "test-2b296",
-        storageBucket: "test-2b296.appspot.com",
-        messagingSenderId: "496593764890",
-        appId: "1:496593764890:web:0a4053a50b394b6ed107a3",
-        measurementId: "${config.measurementId}"
-	}
+	  apiKey: "AIzaSyAHITlgmcGGQ6jADHvLFzWdTBITKV1FiEI",
+	  authDomain: "svelte-fire-tail-temp.firebaseapp.com",
+	  projectId: "svelte-fire-tail-temp",
+	  storageBucket: "svelte-fire-tail-temp.appspot.com",
+	  messagingSenderId: "117783386320",
+	  appId: "1:117783386320:web:fdec9fe8372b4c7e9ee6d5"
+	};
 	
 	initializeApp(firebaseConfig);
 </script>
